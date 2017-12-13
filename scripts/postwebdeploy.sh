234fe2c057ecc89eb08eb54829ce1624e46b3b5f
sfairchild <sfairchild@trinet-hi.com>
2017-12-13 11:27:33 -1000

    updates

--- scripts/postwebdeploy.sh
+++ scripts/postwebdeploy.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 while inotifywait -e modify /var/www/secnodeinfo.txt; do
+	FQDN=$(sed -n '2p' /var/www/secnodeinfo.txt)
+	TADDR=$(sed -n '3p' /var/www/secnodeinfo.txt)
+	apt install socat
+	cd /root 
+	git clone https://github.com/Neilpang/acme.sh.git
+	cd acme.sh
+	./acme.sh --install
+	./acme.sh --issue --standalone -d $FQDN 
+
 	break 
 done
