89083bafcf4b7c530db04cfec268cdd84499bda4
sfairchild <sfairchild@trinet-hi.com>
2017-12-13 14:21:07 -1000

    updates

--- scripts/postwebdeploy.sh
+++ scripts/postwebdeploy.sh
@@ -2,12 +2,10 @@
 while inotifywait -e modify /var/www/secnodeinfo.txt; do
 	FQDN=$(sed -n '2p' /var/www/secnodeinfo.txt)
 	TADDR=$(sed -n '3p' /var/www/secnodeinfo.txt)
-	apt install socat
-	cd /root 
-	git clone https://github.com/Neilpang/acme.sh.git
-	cd acme.sh
-	./acme.sh --install
-	./acme.sh --issue --standalone -d $FQDN 
-
+	apt-get -y update
+	apt-get -y install software-properties-common
+	add-apt-repository -y ppa:certbot/certbot
+	apt-get -y update
+	apt-get -y install python-certbot-apache 
 	break 
 done
